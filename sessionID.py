2ca9c0bcb44336b4429932e01af428765a94b600
AlanHealy-GMIT <G00398793@gmit.ie>
2021-04-28 10:18:51 +0100

    Week09 problem sheet task

--- sessionID.py
+++ sessionID.py
@@ -8,9 +8,9 @@
 # Author: Alan Healy
 # Date Created: 27-APR-2021
 #
-# Reference: https://regex101.com/r/c9Lw9y/1 # This is the SessionID regex
-# Reference: https://pandas.pydata.org/pandas-docs/stable/user_guide/groupby.html
-
+# Reference 1: https://regex101.com/r/c9Lw9y/1 # This is the SessionID regex
+# Reference 2: https://pandas.pydata.org/pandas-docs/stable/user_guide/groupby.html
+# Reference 3: https://stackoverflow.com/questions/39922986/pandas-group-by-and-sum
 
 # import necessary modules
 import pandas as pd
@@ -18,8 +18,9 @@ import regex as re
 import matplotlib.pyplot as plt
 
 # assign log file to a variable
-#logFilename = 'w07_www1_access.log'
-logFilename = '../Prog_CyberSecOps/week09-pandas/data/access.log' # smaller log file for testing
+# using cut down log file with 50 lines from each of the first 3 days, as the plot at the end was illegible with so much data
+logFilename = 'threeDayAccess.log'
+# logFilename = 'w07_www1_access.log' 
 
 # column names to be added to the dataframe
 colNames = ('IP', 'Dash1', 'UserID', 'Time', 'URL', 'Status Code',
@@ -29,17 +30,12 @@ colNames = ('IP', 'Dash1', 'UserID', 'Time', 'URL', 'Status Code',
 df = pd.read_csv(logFilename, sep=' ', header=None, names=colNames)
 
 # remove [] from time
-
-
 def getNewTime(x):
     newTime = re.search('[\w:/]+', x).group()
-    # do your stuff
     return newTime
 
+# uses function above to apply the regex to the 'Time' column
 df['Time'] = df['Time'].apply(getNewTime)
-
-
-#df['Time'] = df['Time'].apply(lambda x: re.search('[\w:/]+', x).group())
 
 # change 'Time' type from object to time, using the format of the time in the log file
 df['Time'] = pd.to_datetime(df['Time'], format='%d/%b/%Y:%H:%M:%S')
@@ -48,34 +44,40 @@ df['Time'] = pd.to_datetime(df['Time'], format='%d/%b/%Y:%H:%M:%S')
 df = df.set_index(['Time'])
 
 # print first row only (for testing)
-#print(df.iloc[0])
+# print(df.iloc[0])
 
 # extract session IDs from URL and save to new column
 # use regex to get sessionID (REF 1), create in normal function, as per lab
-
 def applySessionIDRegex(x):
     findSessionID = re.search('(?:JSESSIONID=)(\S+)', x).group()
     sessionID = re.split('=', findSessionID)[1] # above regex wasn't working, so split on the '=' and chose the end part of the split [1]
     return sessionID
 
-# new column
+# create new column by applying function from above
 df['SessionID'] = df['URL'].apply(applySessionIDRegex)
 
-print(df)
-#newdf = df['2021/02/15 18:00:00':'2021/02/15 18:59:59']
+# groupby by sessionID and sum all the 'Size of Response' to get total data downloaded per sessionID
+# REF 3 (double square brackets came from a comment on the second answer, keeps the object as DataFrame)
+sumSessionIDData = df.groupby(['SessionID'])[['Size of Response']].sum()
+print(sumSessionIDData)
 
-#newdf = df.between_time('23:00', '23:59') # this is times every day
+# extra marks part
+# sums SessionId data per every 1 day
+# the sum values seem the same, as it looks like in this log, no sessionID is used across multiple days,
+# therefore the sum of the data per sessionID per day will be the same as the sum of the data per sessionID
+dailySessionIDData = df.groupby(['SessionID'])[['Size of Response']].resample(rule = '1d').sum()
+print(dailySessionIDData)
 
-#print(newdf)
+# create plot of data
+#sumSessionIDData.plot(marker='o', ls='--', color='r')
+sumSessionIDData.plot(kind='bar', color='r', width=0.5)
 
-# downloadSamples = df['Size of Response'].resample(rule='10Min').mean()
-# print(downloadSamples)
+plt.title("Week 09 Problem Sheet Task - sessionID.py", loc='center')
+plt.xlabel("SessionID")
+plt.ylabel("Size of Response (bytes)")
+plt.legend()
 
-# downloadSamples.plot()
+plt.grid(axis = 'y')
+plt.tight_layout()
 
-# plt.title("readFromLog.py", loc='center')
-# plt.xlabel("X Axis")
-# plt.ylabel("Y Axis")
-# plt.legend()
-
-# plt.show()
+plt.show()
